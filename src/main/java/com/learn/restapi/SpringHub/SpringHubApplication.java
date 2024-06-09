e0e0dda3f4b7479eb0021f0ec7acad51997bb8c7
meAltf <alataf.dce.cs1@gmail.com>
2024-06-09 12:13:43 +0530

    Creating springboot project end to end

---
+++ src/main/java/com/learn/restapi/SpringHub/SpringHubApplication.java
@@ -0,0 +1,16 @@
+package com.learn.restapi.SpringHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan("com.learn.SpringHub.*")
+@ComponentScan({"com.learn.SpringHub.controller", "com.learn.SpringHub.service"})
+public class SpringHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHubApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/restapi/SpringHub/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.learn.restapi.SpringHub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+}
