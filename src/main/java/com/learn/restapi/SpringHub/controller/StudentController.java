f89782497eb8af4f44cc31c749db1249853b57bd
meAltf <alataf.dce.cs1@gmail.com>
2024-06-09 14:47:13 +0530

    @value annotation to read property

--- src/main/java/com/learn/restapi/SpringHub/controller/StudentController.java
+++ src/main/java/com/learn/restapi/SpringHub/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.learn.restapi.SpringHub.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/student/")
 public class StudentController {
 
+    /**
+     * if this spring.application.name unabel to found in application.properties file,
+     * then it takes default value as mentioned below
+     * and doesn't throw any errorrr.
+     */
+    @Value("${spring.application.name:Default Demo App}")
+    private String appName;
+
     @GetMapping("/get")
     public String getStudent(){
-        return "Hello Student";
+        //return "Hello Student";
+        return appName;
     }
 }
