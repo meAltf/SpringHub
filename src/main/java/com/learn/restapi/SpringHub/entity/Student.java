9f4576b61f4bd4589c4bf7ea64cd003761a702b9
meAltf <alataf.dce.cs1@gmail.com>
2024-06-11 06:18:53 +0530

    for auto increment, added strategy

--- src/main/java/com/learn/restapi/SpringHub/entity/Student.java
+++ src/main/java/com/learn/restapi/SpringHub/entity/Student.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
