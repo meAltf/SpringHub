a0e443676b178cefe6c23075690d5b494850195f
meAltf <alataf.dce.cs1@gmail.com>
2024-06-11 06:18:01 +0530

    created requestDTO for post API

---
+++ src/main/java/com/learn/restapi/SpringHub/request/CreateStudentRequest.java
@@ -0,0 +1,17 @@
+package com.learn.restapi.SpringHub.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateStudentRequest {
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
