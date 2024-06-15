843bc180bc6fd34b242dea889ac98339014672c7
meAltf <alataf.dce.cs1@gmail.com>
2024-06-15 19:06:59 +0530

    created entity for address table

---
+++ src/main/java/com/learn/restapi/SpringHub/entity/Address.java
@@ -0,0 +1,23 @@
+package com.learn.restapi.SpringHub.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String streetName;
+
+    @Column(name = "city")
+    private String cityName;
+}
