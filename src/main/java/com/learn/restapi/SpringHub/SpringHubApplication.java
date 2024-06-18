4f56d188e70993cbe0791e6c29f82cdcb849391b
meAltf <alataf.dce.cs1@gmail.com>
2024-06-18 22:04:26 +0530

    scheduler

--- src/main/java/com/learn/restapi/SpringHub/SpringHubApplication.java
+++ src/main/java/com/learn/restapi/SpringHub/SpringHubApplication.java
@@ -3,10 +3,12 @@ package com.learn.restapi.SpringHub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@ComponentScan("com.learn.SpringHub.*")
 //@ComponentScan({"com.learn.SpringHub.controller", "com.learn.SpringHub.service"})
+@EnableScheduling
 public class SpringHubApplication {
 
     public static void main(String[] args) {
